f5346b44df236ae298344f262027332fe5541a29
silver21br <silverja@gmail.com>
2021-03-20 18:20:59 -0300

    Primeiro commit feito por Silvestre Resetando as mudanças feitas por Silvestre

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -2,13 +2,14 @@ package one.digitalinnovation.digionebank.testes
 
 import one.digitalinnovation.digionebank.Gerente
 
-
 fun main() {
 
-    val maria = Gerente("JMaria do Carmo", "1234567899", 5000.0)
+    val maria = Gerente("JMaria do Carmo", "1234567899", 5000.0, "senha123")
     //val joao = Funcionario(nome: "Jether Rodrigues", cpf: "1234567899", BigDecimal.valueOf(val: 2000.0))
     //println(joao.nome)
     //println(joao.cpf)
     //println(joao.salario)
     imprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
